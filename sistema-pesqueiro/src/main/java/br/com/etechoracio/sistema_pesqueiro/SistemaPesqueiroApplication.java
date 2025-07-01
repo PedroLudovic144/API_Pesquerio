ee69b3b2c2f55403aa88c3d84e64d5046f4f3794
PedroLudovic144 <pedroludovic5@gmail.com>
2025-07-01 17:56:57 -0300

    fix: The correct project now

---
+++ sistema-pesqueiro/src/main/java/br/com/etechoracio/sistema_pesqueiro/SistemaPesqueiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.sistema_pesqueiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPesqueiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPesqueiroApplication.class, args);
+	}
+
+}
---
+++ sistema-pesqueiro/src/test/java/br/com/etechoracio/sistema_pesqueiro/SistemaPesqueiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.sistema_pesqueiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPesqueiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
